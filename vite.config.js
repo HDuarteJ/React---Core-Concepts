c53a7dbdecfd2a9afdccc44051daea70420d385c
Henrique Duarte <h.duarte.j7@gmail.com>
2024-10-10 14:50:29 -0400

    Property base from vite.config fixed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/https://hduartej.github.io/react---Core-Concepts"
+  base: "/react---Core-Concepts"
 });
